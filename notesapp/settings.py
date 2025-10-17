5e68c7434f91bcfc24455af076191088f97673b7
Aftab khan <142973090+aftabkhan77@users.noreply.github.com>
2025-10-17 23:59:06 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-    DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
